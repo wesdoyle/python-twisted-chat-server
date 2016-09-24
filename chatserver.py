9ce8e84e79492cc8935c25c3a5656c403a1ec783
wesdoyle <wesdoyle08@gmail.com>
2016-09-24 11:09:39 -0500

    implement clint to add color to chat, list users upon joining

--- chatserver.py
+++ chatserver.py
@@ -1,6 +1,7 @@
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
+from clint.textui import colored
 
 class ChatProtocol(LineReceiver):
     def __init__(self, factory):
@@ -9,36 +10,52 @@ class ChatProtocol(LineReceiver):
         self.state = "REGISTER"
 
     def connectionMade(self):
-        self.sendLine("What's your name?")
+        banner = colored.blue("""
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        ####### Successfully Connected to the Chat Server #######
+        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        """)
+        
+        self.sendLine(banner)
+        self.sendLine("Choose a name for this session:")
 
     def connectionLost(self, reason):
+        leftMsg = colored.red('%s has left the channel.' % (self.name,))
         if self.name in self.factory.users:
             del self.factory.users[self.name]
-            self.broadcastMessage("%s has left the channel." % (self.name,))
+            self.broadcastMessage(leftMsg)
 
     def lineReceived(self, line):
         if self.state == "REGISTER":
             self.handle_REGISTER(line)
         else:
             self.handle_CHAT(line)
+
     def handle_REGISTER(self, name):
        if name in self.factory.users:
-           self.sendLine("Name taken; please choose another.")
+           self.sendLine("Sorry, that name is taken. Choose a different name.")
            return
-       self.sendLine("Welcome, %s!" % (name,))
-       self.broadcastMessage("%s has joined the channel." % (name,))
+       
+       welcomeMsg = colored.green('Welcome to the chat, %s.' % (name,))
+       otherUsers = colored.blue('Participants in chat: %s ' % (", ".join(self.factory.users)))
+       joinedMsg = colored.cyan('%s has joined the chanel.' % (name,))
+       
+       self.sendLine(welcomeMsg)
+       self.broadcastMessage(joinedMsg)
+       self.sendLine(otherUsers)
        self.name = name
        self.factory.users[name] = self
        self.state = "CHAT"
 
     def handle_CHAT(self, message):
        message = "<%s> %s" % (self.name, message)
-       self.broadcastMessage(message)
+       self.broadcastMessage(colored.magenta(message))
 
     def broadcastMessage(self, message):
        for name, protocol in self.factory.users.iteritems():
            if protocol != self:
-               protocol.sendLine(message)
+               protocol.sendLine(colored.white(message))
+
 
 class ChatFactory(Factory):
     def __init__(self):
